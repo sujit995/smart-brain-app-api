50ed5dcc8fa683afbaf9729c8e82e9743d51b3d8
sujit995 <sujitmishra402@gmail.com>
2021-03-08 09:51:02 +0530

    add port for heroku

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image').default;
+const port = process.env.PORT || 8000;
+
 
 const db = knex({
   // connect to your own database here
@@ -32,6 +34,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3000, () => {
-  console.log('app is running on port 3000');
+app.listen(port, () => {
+  console.log(`app is running on port ${port}`);
 })

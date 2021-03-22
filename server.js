0e035f9acb7ca81be9bb431d8bbcf8c59e768533
sujit995 <sujitmishra402@gmail.com>
2021-03-22 22:31:47 +0530

    port change

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(port, () => {
-  console.log(`app is running on port ${port}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.POST}`);
 })
